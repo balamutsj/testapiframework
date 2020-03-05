95b73f9b37dc12ec8a9317978e66f9e8b12ec81d
Igor Sanzharevskiy <balamutsj@gmail.com>
2020-03-05 11:42:41 +0300

    Update ApiHeader.java
    default header update

--- src/test/java/ApiData/ApiHeader.java
+++ src/test/java/ApiData/ApiHeader.java
@@ -8,7 +8,7 @@ public class ApiHeader {
     public static Map<String, String> defaultHeader(){
         HashMap<String, String> headers = new HashMap<String, String>();
         headers.put("Accept", "*/*");
-        //headers.put("Content-type", "application/json");
+        headers.put("Content-type", "application/json");
         return headers;
     }
 }
